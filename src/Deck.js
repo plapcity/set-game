0599b34ed4032c0fe7abc421f791a6855b2b0b7b
Perri Lapidus <perri@wondersauce.com>
2018-03-07 18:03:50 -0500

    finally happy with the styles

--- src/Deck.js
+++ src/Deck.js
@@ -9,16 +9,9 @@ class Deck extends React.Component {
 	}
 
 	render(){
-		const numCards = this.props.deck.length;
-		const cards = this.props.deck.map((card, index) => <Card key={card.id} card={card}/>
-	)
 		return(
-			<div className='deck'>
-				<h4>There are {numCards} cards in the deck</h4>
-				{cards}
-			</div>
+			<h3 className='deck'>There are {this.props.deck.length} cards in the deck</h3>
 		)
-
 	}
 }
 
